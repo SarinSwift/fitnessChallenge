58b7db6e63f3dc62373fe34665c295a831da4286
SarinSwift <sarinyaswift@gmail.com>
2019-01-07 15:28:28 -0800

    added chooseEmojiViewCOntroller with 7 different emojis

--- fitnessChallenge/viewControllers/ChooseEmojiViewController.swift
+++ fitnessChallenge/viewControllers/ChooseEmojiViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ChooseEmojiViewController: UIViewController {
     
+    // Icon made by Freepik from www.flaticon.com 
+    let emojiImages = ["smile", "inlove", "happy", "neutral", "thinking", "cry", "mad"]
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pickEmojiButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.register(emojiImageCollectionViewCell.nib, forCellWithReuseIdentifier: emojiImageCollectionViewCell.identifier)
+        
+        
         collectionView.allowsMultipleSelection = false
     }
     
@@ -25,6 +32,39 @@ class ChooseEmojiViewController: UIViewController {
         
         alert.addAction(yesAction)
         alert.addAction(cancelAction)
+    }
+    
+}
+
+extension ChooseEmojiViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojiImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: emojiImageCollectionViewCell.identifier, for: indexPath) as! emojiImageCollectionViewCell
+        cell.setupImage(image: UIImage(named: emojiImages[indexPath.row])!)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        return CGSize(width: collectionViewWidth/5, height: collectionViewWidth/5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderWidth = 2
+        cell?.layer.cornerRadius = 38
+        cell?.layer.borderColor = #colorLiteral(red: 0.928014338, green: 0.1565714478, blue: 0.1036314741, alpha: 1)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell  = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderWidth = 0
+        
     }
     
 }
---
+++ fitnessChallenge/viewControllers/emojiImageCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  emojiImageCollectionViewCell.swift
+//  fitnessChallenge
+//
+//  Created by Sarin Swift on 1/7/19.
+//  Copyright © 2019 sarinswift. All rights reserved.
+//
+
+import UIKit
+
+class emojiImageCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "cellId"
+
+    @IBOutlet weak var emojiImage: UIImageView!
+    
+    
+    // this static var allows to access this variable in ChooseEmojiVIewContorller
+    static var nib: UINib {
+        return UINib(nibName: String(describing: self), bundle: nil)
+    }
+    
+    func setupImage(image: UIImage) {
+        self.emojiImage.image = image
+    }
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+
+}
