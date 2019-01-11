de1daf706ae471faefac24371ac49d1f9640ce84
SarinSwift <sarinyaswift@gmail.com>
2019-01-11 08:02:18 -0800

    programmatic collectionView on infoViewCOntroller

--- fitnessChallenge/viewControllers/InfoViewController.swift
+++ fitnessChallenge/viewControllers/InfoViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 import SafariServices
 
-class InfoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class InfoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     let exerImages = ["arms", "legs", "abs", "chest", "back", "shoulders", "calves"]
     
     @IBOutlet weak var infoLabel: UILabel!
-    @IBOutlet weak var collectionView: UICollectionView!
+//    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
     }
     func setupCollectionView() {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.itemSize = CGSize(width: view.frame.width, height: 700)
+        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.register(InfoExerciseCell.self, forCellWithReuseIdentifier: "infoCellId")
+        collectionView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
-        collectionView.register(InfoExerciseCell.self, forCellWithReuseIdentifier: "infoCellId") 
+        self.view.addSubview(collectionView)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -35,14 +41,24 @@ class InfoViewController: UIViewController, UICollectionViewDelegate, UICollecti
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "infoCellId", for: indexPath) as! InfoExerciseCell
 //        cell.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         cell.exerciseImageView.image = UIImage(named: exerImages[indexPath.row])
+        cell.exerciseLabel.text = exerImages[indexPath.row]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // redirects to the exercises web page
         if let url = NSURL(string: "https://wger.de/en/exercise/overview/"){
             let svc = SFSafariViewController(url: url as URL)
             present(svc, animated: true, completion: nil)
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 135, height: 135)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 70, left: 8, bottom: 5, right: 8)
     }
     
 }
@@ -56,14 +72,31 @@ class InfoExerciseCell: UICollectionViewCell {
         return imageView
     }()
     
+    let exerciseLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.contentMode = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addViews()
+    }
+    
+    func addViews() {
         addSubview(exerciseImageView)
+        addSubview(exerciseLabel)
+        
         NSLayoutConstraint(item: exerciseImageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 1).isActive = true
         NSLayoutConstraint(item: exerciseImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 1).isActive = true
         NSLayoutConstraint(item: exerciseImageView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 1).isActive = true
         NSLayoutConstraint(item: exerciseImageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 1).isActive = true
+        
+        NSLayoutConstraint(item: exerciseLabel, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 1).isActive = true
+        NSLayoutConstraint(item: exerciseLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
